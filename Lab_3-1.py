6aece69164f8dc61c6cb13dc496ee1e9b0c4c84e
p24jmorris <p24jmorris@fairfieldprep.org>
2023-09-27 11:36:31 -0400

    Lab 3-1 submission

--- Lab_3-1.py
+++ Lab_3-1.py
@@ -3,6 +3,10 @@ Create a Python file named lab_3-1.py
 In a video game, you can slay the dragon only if your magic strength is 90 or higher and you shield is charged to at least 75. 
 Assume both values will be provided by the user as integers. Modify the code below to form a simpler to understand conditional that uses logical operators.
 
+a= input("magic_charge")
+a = int (a)
+b= input("shield_charge")
+b = int (b)
 
 if not ((magic_charge >= 90) and (shield_charge >= 75)):
 	print ("The dragon burns you to a crisp.")
